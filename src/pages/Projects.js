e426c9a3c9902efa7dcd2817f87c146a5297238e
JWalters <joshwalters34@gmail.com>
2021-06-08 10:50:04 -0400

    updated project page to map through project.json data

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,5 @@
-import React from "react";
-import projects from "../projects.json"
+import React, { Component } from "react";
+import projects from "../projects.json";
 
 // {projects.map(project => (
 //   key={project.title}
@@ -9,45 +9,52 @@ import projects from "../projects.json"
 //   repository={project.repository}
 //   )};
 
-function Projects({ title, website, repository, description, image}) {
-  return (
-    
-    <div className="card mb-3" style={{width: '90%'}}>
-    <div className="row no-gutters">
-      <div className="col-md-4">
-        <img
-          className="bd-placeholder-img"
-          width="100%"
-          height="250"
-          alt={title} src={image}></img>
-      </div>
-      <div className="col-md-8">
-        <div className="card-body">
-          <h5 className="card-title">title {title}</h5>
-          <p className="card-text">
-            {description} description
-          </p>
-          <p className="card-text">
-            Visit my {website} 
-          </p>
-          <p className="card-text">
-          View my {repository} 
-          </p>
+class Projects extends Component {
+  state = {
+    projects,
+  };
+  render() {
+    return (
+      <div className="container">
+      {this.state.projects.map((project) => (
+        
+      <div className="card mb-3" style={{ width: "90%" }}>
+        <div className="row no-gutters">
+
+              <div className="col-md-4">
+                <img
+                  className="bd-placeholder-img"
+                  width="100%"
+                  height="250"
+                  alt={project.title}
+                  src={project.image}
+                ></img>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">title {project.title}</h5>
+                  <p className="card-text">{project.description} description</p>
+                  <p className="card-text">Visit my {project.website}</p>
+                  <p className="card-text">View my {project.repository}</p>
+                </div>
+              </div>
+            </div>
           
-        </div>
       </div>
-    </div>
-  </div>
-);
+      ))}
+      ;
+      </div>
+    );
+  }
   //   <div className="card">
-   
+
   //   <div className="content">
-      
+
   //       <h1>
-  //         <strong>{title}</strong> 
+  //         <strong>{title}</strong>
   //       </h1>
   //       <p>
-  //         <strong>{description}</strong> 
+  //         <strong>{description}</strong>
   //       </p>
   //       <p>
   //         <strong>Location:View my {website}
@@ -59,8 +66,6 @@ function Projects({ title, website, repository, description, image}) {
   //   </div>
   //   </div>
   // </div>
-
 }
-
 
 export default Projects;
